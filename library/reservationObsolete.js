684e54af2a4ef19becfd87732146700dba6fe9c8
misuke007 <misuketiana@gmail.com>
2024-03-05 12:15:15 +0100

    chore(ReservationObsolete) : mise à jour du code

--- library/reservationObsolete.js
+++ library/reservationObsolete.js
@@ -1,19 +1,24 @@
 
-const {Reservation} = require('../models')
+const { Reservation , Livre } = require('../models')
 const { Op } = require('sequelize')
+
 
 // cette fonction permet de vérifier si une réservation est obsolète ou non. si oui => la reservation sera supprimer
 
 
-const resaObsolete = async() =>{
+const resaObsolete = async () => {
 
-    const data = await Reservation.findAll({where:{date_recuperation : {[Op.lt] : new Date}}})
+    const dataReservation = await Reservation.findAll({ where: { date_recuperation: { [Op.lt]: new Date } } })
+    let dataLivre
 
-    if(data.length != 0){
+    if (dataReservation.length != 0) {
 
-        for(item of data){ 
+        for (item of dataReservation) {
 
-            await Reservation.destroy({where:{id : item.id}})
+            await Reservation.destroy({ where: { id: item.id } })
+            dataLivre = await Livre.findOne({ where: { id: item.LivreId } })
+            dataLivre.exemplaire += 1
+            await dataLivre.save()
         }
 
     }
